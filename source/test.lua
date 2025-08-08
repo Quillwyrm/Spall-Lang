8151ac922ab0485f0a451af13625f75c585f11f9
Jacob Holland <thejacobholland@gmail.com>
2025-08-09 00:18:14 +1000

    Update test.lua

--- source/test.lua
+++ source/test.lua
@@ -3,17 +3,17 @@
 local core = require("./spallcore")
 
 -- state
-local _context      = core._context
+local _context = core._context
 
 -- draw prims
-local _Rect         = core._Rect
-local _Blit         = core._Blit
-local _Circ         = core._Circ
-local _Line         = core._Line
-local _draw         = core._draw
+local _Rect = core._Rect
+local _Blit = core._Blit
+local _Circ = core._Circ
+local _Line = core._Line
+local _draw = core._draw
 
 -- core util
-local _commitTemp   = core._commitTemp
+local _commitTemp = core._commitTemp
 
 -- palette
 local blk = _context.palette.blk
@@ -22,14 +22,12 @@ local red = _context.palette.red
 local grn = _context.palette.grn
 
 -- debug/test
-local log           = core.test_logBufferToConsole
+local log = core.test_logBufferToConsole
 --------------------------------------------------------
-
 
 -- Create destination tile (unpositioned base buffer)
 -- # demo 16 16
 _context.tiles["demo"] = core._PixelBuffer(16, 16)
-
 
 -- line grn 8 1 1 8
 _context.temp = _Line(grn, 8, 1, 1, 8)
@@ -67,11 +65,9 @@ _commitTemp("demo")
 _context.temp = _Line(grn, 1, 1, 16, 16)
 _commitTemp("demo")
 
-
 -- Output result -- implicit in final dsl
 log("demo", _context.tiles["demo"])
 core.test_outputBufferToPPM(_context.tiles["demo"], "../output/demo.ppm", _context.colors)
-
 
 --[[ .spl equivelent
 
